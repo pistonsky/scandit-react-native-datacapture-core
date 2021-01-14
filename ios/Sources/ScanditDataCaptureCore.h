eb90bf7a7456171fc5213583de1ab544185e8c7a
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2021-01-15 00:55:37 +0300

    Remove support for iOS as it breaks build process

--- ios/Sources/ScanditDataCaptureCore.h
+++
@@ -1,7 +0,0 @@
-/*
- * This file is part of the Scandit Data Capture SDK
- *
- * Copyright (C) 2020- Scandit AG. All rights reserved.
- */
-
-__attribute__((visibility("default"))) extern void inline LogWarn(NSString *message);
